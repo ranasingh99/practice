150b9e9257ad3acdd307828552365c10af8c42eb
ranasingh99 <sanketverma852@gmail.com>
2023-07-26 18:26:17 +0530

    delete button added

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
-// function saveToLocalStorage(e){
-//   e.preventDefault();
-//   const name = e.target.username.value;
-//   const emailId = e.target.useremail.value;
-//   const phonenumber = e.target.usercontact.value;
-//   const calltime = e.target.datetime.value;
-//   const myObj={
-//     username:name,
-//     emailId:emailId,
-//     phonenumber:phonenumber,
-//     calltime:calltime
-//   }
-//   let myObj_serialised = JSON.stringify(myObj);
-//   localStorage.setItem(myObj.emailId,myObj_serialised);
-  // localStorage.setItem('customer name',name);
-  // localStorage.setItem('customer email',emailId);
-  // localStorage.setItem('customer conatct_number',phonenumber);
-  // localStorage.setItem('Time of call',calltime);
+function saveToLocalStorage(e){
+  e.preventDefault();
+  const name = e.target.username.value;
+  const emailId = e.target.useremail.value;
+  const phonenumber = e.target.usercontact.value;
+  const calltime = e.target.datetime.value;
+  const myObj={
+    username:name,
+    emailId:emailId,
+    phonenumber:phonenumber,
+    calltime:calltime
+  }
+  let myObj_serialised = JSON.stringify(myObj);
+  localStorage.setItem(myObj.emailId,myObj_serialised);
+  showUsersOnScreen(myObj);
 
-//showUsersOnScreen(myObj);
+}
 
-//}
-
-function showUsersOnScreen(event){
-  event.preventDefault();
-  let name = event.target.username.value;
-  let email = event.target.useremail.value;
-  console.log(name+" "+email);
+function showUsersOnScreen(obj){
   let parentElement= document.getElementById('listOfItems');
   let childElement = document.createElement('li');
-  childElement.textContent = name+" "+email;
+  childElement.textContent = obj.username+" "+obj.emailId;
+  // add delete button
+ let deleteButton = document.createElement('input');
+ deleteButton.type = 'button';
+ deleteButton.value = 'Delete';
+ deleteButton.className = 'btn2';
+ deleteButton.onclick = ()=>{
+  localStorage.removeItem(obj.emailId);
+  parentElement.removeChild(childElement);
+ }
+ childElement.appendChild(deleteButton);
   parentElement.appendChild(childElement);
 }
