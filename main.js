09856b96e6dad094b2cffaa8647378ff3e2dfad9
ranasingh99 <sanketverma852@gmail.com>
2023-10-15 17:18:01 +0530

    data is saved on cloud using axios network call

--- main.js
+++ main.js
@@ -5,29 +5,35 @@ function saveToLocalStorage(e){
   const phonenumber = e.target.usercontact.value;
   const calltime = e.target.datetime.value;
   const myObj={
-    username,
+    name,
     emailId,
     phonenumber,
     calltime
   }
-  axios.post('https://crudcrud.com/api/9346a909c2374d7b93649fa58002c0ed/appointmentdata',obj)
+  axios.post('https://crudcrud.com/api/ce7940cb620a49ef8603db37ae5c17d3/appointmentData',myObj)
+  .then((res)=>{
+    showUsersOnScreen(res.data)
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
   let myObj_serialised = JSON.stringify(myObj);
   //localStorage.setItem(myObj.emailId,myObj_serialised);
-  showUsersOnScreen(myObj);
+  //showUsersOnScreen(myObj);
 
 }
 
 function showUsersOnScreen(obj){
   let parentElement= document.getElementById('listOfItems');
   let childElement = document.createElement('li');
-  childElement.textContent = obj.username+" "+obj.emailId;
+  childElement.textContent = obj.name+" "+obj.emailId;
   // add delete button
  let deleteButton = document.createElement('input');
  deleteButton.type = 'button';
  deleteButton.value = 'Delete';
  deleteButton.className = 'btn2';
  deleteButton.onclick = ()=>{
-  localStorage.removeItem(obj.emailId);
+  //localStorage.removeItem(obj.emailId);
   parentElement.removeChild(childElement);
  }
 
@@ -37,7 +43,7 @@ function showUsersOnScreen(obj){
  editButton.value = 'Edit';
  editButton.className = 'btn3';
  editButton.onclick = ()=>{
-    localStorage.removeItem(obj.emailId);
+   // localStorage.removeItem(obj.emailId);
     parentElement.removeChild(childElement);
     document.getElementById('name').value = obj.username;
     document.getElementById('email').value = obj.emailId;
